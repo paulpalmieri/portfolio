9ca8b703fbcdfd024e656095ca445a70acc0fdfa
Paul Palmieri <palmirouze@gmail.com>
2019-07-10 17:27:14 -0400

    Added language fetching from Github's api

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const fetchTopic = repo_name => {
     },
     success: function(res) {
       let tag_str = "";
-      console.log(res.names.length);
-      console.table(res.names);
+      // console.log(res.names.length);
+      // console.table(res.names);
       if (res.names.length > 0) {
         res.names.forEach(element => {
           tag_str += `<div class="topics">${element}</div>`;
@@ -24,12 +24,44 @@ const fetchTopic = repo_name => {
   });
 };
 
+const fetchLanguages = repo_name => {
+  $.ajax({
+    url: `https://api.github.com/repos/paulpalmieri/${repo_name}/languages`,
+    type: "GET",
+    async: "false",
+    success: function(res) {
+      let tag_str = "";
+      if (!jQuery.isEmptyObject(res)) {
+        const keys = Object.keys(res);
+
+        const total = keys.reduce(function(total, element) {
+          console.log(res[element] + ":  " + element);
+          return total + res[element];
+        }, 0);
+
+        console.log(total);
+
+        keys.forEach(element => {
+          const percentage = Number(((res[element] / total) * 100).toFixed(1));
+          tag_str += `<div class="language">${element}: ${percentage}%</div>`;
+        });
+        $(`.language_container[data-name="${repo_name}"]`).append(tag_str);
+        // console.log(tag_str);
+      }
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
+};
+
 $(document).ready(function() {
   // fetch all my repos
   $.ajax({
     url: "https://api.github.com/users/paulpalmieri/repos",
     type: "GET",
     success: function(res) {
+      console.table(res);
       res
         .sort(function(a, b) {
           // sort by recently updated
@@ -54,9 +86,11 @@ $(document).ready(function() {
             : "No description for this project"
         }</p>
         <div class="topics_container" data-name="${el.name}"></div>
+        <div class="language_container" data-name="${el.name}"></div>
             </div>`;
           $("#github-container").append(proj_tag);
           fetchTopic(el.name);
+          // fetchLanguages(el.name);
         });
     },
     error: function(err) {
