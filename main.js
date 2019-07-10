7e2f69c1c88ee4d0b426f149bb316215043f1276
Paul Palmieri <palmirouze@gmail.com>
2019-07-10 11:56:14 -0400

    Added hidden color picker, hidden extra link for projects, style changes

--- main.js
+++ main.js
@@ -9,16 +9,26 @@ $(document).ready(function() {
         .sort(function(a, b) {
           return a.updated_at > b.updated_at ? -1 : 1;
         })
-        .forEach(el =>
-          $("#github-container").append(`<div class="project">
-                        <a href=${el.svn_url}>${el.name.toLowerCase()}</a>
-                        <p>${
-                          el.description !== null
-                            ? el.description
-                            : "No description for this project"
-                        }</p>
-                    </div>`)
-        );
+        .forEach(function(el) {
+          const lc_name = el.name.toLowerCase();
+          const has_gitpages = el.has_pages
+            ? `<a class="direct-access" href="http://paulpalmieri.github.io/${lc_name}/">Hosted on <i class="fa fa-github"></i></a>`
+            : "";
+          const toggle_git_pages = false;
+
+          console.log(has_gitpages);
+          const proj_tag = `<div class="project">
+        <a href=${el.svn_url}>${lc_name}</a> ${
+            toggle_git_pages ? has_gitpages : ""
+          }
+        <p>${
+          el.description !== null
+            ? el.description
+            : "No description for this project"
+        }</p>
+            </div>`;
+          $("#github-container").append(proj_tag);
+        });
 
       //   const tags = res.forEach(el => {
       //     const display_tag = `<div class="single_project">
@@ -40,4 +50,8 @@ $(document).ready(function() {
       console.log(err);
     }
   });
+});
+
+$("input").on("change", function() {
+  document.documentElement.style.setProperty(`--${this.name}`, this.value);
 });
